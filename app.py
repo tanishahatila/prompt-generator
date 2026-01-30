4bdf6f867bbf3b7d33a0073d0cd2b9461795bc1e
Tanisha Hatila <tanishahatilahatila@gmail.com>
2026-01-30 17:38:39 +0530

    made env veriable

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
-GOOGLE_REDIRECT_URI = "https://prompt-generator-six-lake.vercel.app/auth/callback"
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 
 # =============================
 # Database Model
