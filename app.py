e0d6a06b537a519f62d64be22156863d9a77c3ed
Tanisha Hatila <tanishahatilahatila@gmail.com>
2026-01-30 17:09:16 +0530

    url changed

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
-GOOGLE_REDIRECT_URI = "http://localhost:5000/auth/callback"
+GOOGLE_REDIRECT_URI = "https://prompt-generator-six-lake.vercel.app/auth/callback"
 
 # =============================
 # Database Model
